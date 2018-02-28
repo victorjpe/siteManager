7efac3c9c6421c2916f794aeb2d1d66463990d2a
victorjpe <victorjosepe@gmail.com>
2018-02-28 23:19:22 +0530

    add camera across the app

--- src/pages/before-work/before-work.ts
+++ src/pages/before-work/before-work.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the BeforeWorkPage page.
@@ -15,11 +16,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BeforeWorkPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
+  }
+
+  picture: any;
+  options: CameraOptions = {
+    quality: 90,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BeforeWorkPage');
+  }
+
+  takePicture(){
+    this.camera.getPicture(this.options).then((imageData) => {
+      this.picture = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {});
   }
 
 }
